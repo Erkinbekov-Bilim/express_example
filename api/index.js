6fc7730ed3cc159c5738c3466194d6ffe8ab3a1e
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-02-13 18:11:54 +0600

    add Express API with Vigenere encode/decode

--- api/index.js
+++ api/index.js
@@ -1,16 +1,27 @@
-import { PORT } from './constants/constants.js';
-import express from 'express';
+const express = require('express');
+const { PORT, PASSWORD } = require('./constants/constants.js');
+const Vigenere = require('caesar-salad').Vigenere;
 
 const app = express();
-
 app.get('/', (req, res) => {
-  const text = req.params.text;
   res.send(`<p classname = "text">Backend api</p>`);
 });
 
 app.get('/:text', (req, res) => {
   const text = req.params.text;
   res.send(`<p classname = "text">${text}</p>`);
+});
+
+app.get('/encode/:data', (req, res) => {
+  const data = req.params.data;
+  const encodedData = Vigenere.Cipher(PASSWORD).crypt(data);
+  res.send(`<p classname = "text">${encodedData}</p>`);
+});
+
+app.get('/decode/:data', (req, res) => {
+  const data = req.params.data;
+  const decodedData = Vigenere.Decipher(PASSWORD).crypt(data);
+  res.send(`<p classname = "text">${decodedData}</p>`);
 });
 
 app.use((req, res) => {
